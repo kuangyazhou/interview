5f290c781027ca93fe579af0b8605c32df80626f
kuangyazhou <18707174713@139.com>
2017-08-05 09:40:14 +0800

    ''

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 console.log('fuck the king!!!');
 
-
 // 实现一个spacify函数，返回字符串结果，中间增加空格
 function spacify(fName) {
     return fName.split('').join(' ');
@@ -13,3 +12,36 @@ String.prototype.spacify = function() {
 
 
 //函数声明和函数表达式，作用域提升，可以在声明前引用
+
+//数组去重,将方法赋于Array原型链上
+
+Array.prototype.repeat = function() {
+    var original = this;
+    if (original.length < 1) return;
+    var data = [];
+    for (i = 0; i < original.length; i++) {
+        if (data.indexOf(original[i]) == -1) {
+            data.push(original[i]);
+        };
+    }
+    return data;
+}
+
+var a = [1, 3, 2, 2, 3, 3, 4, 6, 6];
+console.log(a.repeat());
+
+
+// 运行上下文
+var User = {
+    count: 1,
+    getCount: function() {
+        return this.count;
+    }
+}
+console.log(User.getCount());
+var func = User.getCount();
+console.log(this, func());
+// 输出结果为1和undefined func被调用时，运行上下文为window，不是User;
+
+// 原型绑定，将user作为运行上下文;
+var func = User.getCount().bind(User);
