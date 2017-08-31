948883403e754391db03a6b09a69527ae535767a
kuangyazhou <18707174713@139.com>
2017-08-31 11:12:43 +0800

    dayily update

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ Array.prototype.repeat = function() {
 var a = [1, 3, 2, 2, 3, 3, 4, 6, 6];
 console.log(a.repeat());
 
+//es6语法去重 set生成一个不重复对象
+
+console.log(new Set(a));
 
 // 运行上下文
 var User = {
@@ -39,12 +42,12 @@ var User = {
     }
 }
 console.log(User.getCount());
-var func = User.getCount();
-console.log(this, func());
+// var func = User.getCount();
+// console.log(this, func());
 // 输出结果为1和undefined func被调用时，运行上下文为window，不是User;
 
 // 原型绑定，将user作为运行上下文;
-var func = User.getCount().bind(User);
+// var func = User.getCount().bind(User);
 
 //综合类知识
 function Foo() {
@@ -68,6 +71,33 @@ new Foo().getName();
 new new Foo().getName();
 
 
-//arguments 转Array
+//es6;
+(function() {
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+})();
 
-var arrArgs = Array.prototype.splice.call(arguments);
+
+
+
+//arguments 转Array
+function transform() {
+    var arrArgs = Array.prototype.splice.call(arguments);
+}
+
+
+function Point() {
+    setTimeout(() => {
+        console.log(('id', this.id));
+    }, 1000);
+}
+
+var id = 21;
+
+Point.call(({ id: 42 }));
+// 42;
+// 箭头函数导致this总是指向函数定义生效时所在的对象（本例是{id: 42}），所以输出的是42
+//this 指向调用该方法的对象,call apply bind ，用来改变函数运行时的this指向;
+
+//对于箭头函数而言，函数内的this对象就是定义时所在的对象，而不是使用时所在的对象;
